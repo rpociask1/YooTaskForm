8696a3f831371d8885b948f5d491bc11bc350e4d
rpociask1 <r.pociask@wp.pl>
2020-02-28 20:10:47 +0100

    SignUp Page

--- YooTaskForm/Pages/LoginPage.xaml.cs
+++ YooTaskForm/Pages/LoginPage.xaml.cs
@@ -15,11 +15,23 @@ namespace YooTaskForm.Pages
         public LoginPage()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this,false);
+               
         }
 
         private void BtnLogin_Clicked(object sender, EventArgs e)
         {
-            //
+            Navigation.PushAsync(new SignUpPage());
+        }
+
+        private void TapSignUp_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SignUpPage());
+        }
+
+        private void TapSignUp_OnTapped(object sender, EventArgs e)
+        {
+             Navigation.PushAsync(new SignUpPage());
         }
     }
 }
---
+++ YooTaskForm/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace YooTaskForm.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void BtnSign_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
